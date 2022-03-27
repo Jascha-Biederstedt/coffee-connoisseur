ffdd287732da5122e67ca488e185d5129d0d0aa1
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-03-27 12:11:35 +0200

    Style the hero container

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const CoffeeStore = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <div>id: {id}</div>;
+  return (
+    <div>
+      id: {id}
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+    </div>
+  );
 };
 
 export default CoffeeStore;
