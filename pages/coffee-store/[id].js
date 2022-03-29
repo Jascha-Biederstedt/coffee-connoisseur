31bba0855605bde6a033e4e1b379297f67951e9f
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-03-29 12:13:45 +0200

    Implement SSG on dynamic route

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -2,7 +2,30 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(coffeeStore => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '0' } },
+      { params: { id: '1' } },
+      { params: { id: '300' } },
+    ],
+    fallback: false,
+  };
+}
+
+const CoffeeStore = ({ coffeeStore }) => {
   const router = useRouter();
   const { id } = router.query;
 
@@ -12,6 +35,8 @@ const CoffeeStore = () => {
       <Link href="/">
         <a>Back to Home</a>
       </Link>
+      <p>{coffeeStore.name}</p>
+      <p>{coffeeStore.address}</p>
     </div>
   );
 };
