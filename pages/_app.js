10aaa6fc11e8a424de9edae99d4cebeac1993fcc
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-05 14:59:59 +0200

    Create store reducer in context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 import '../styles/globals.css';
 
 const StoreContext = createContext();
+
+const ACTION_TYPES = {
+  SET_LATITUDE: 'SET_LATITUDE',
+  SET_LONGITUDE: 'SET_LONGITUDE',
+  SET_COFFEE_STORES: 'SET_COFFEE_STORES',
+};
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LATITUDE:
+      return { ...state, latitude: action.payload.latitude };
+    case ACTION_TYPES.SET_LONGITUDE:
+      return { ...state, latitude: action.payload.longitude };
+    case ACTION_TYPES.SET_COFFEE_STORES:
+      return { ...state, coffeeStores: action.payload.coffeeStores };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
 const StoreProvider = ({ children }) => {
   const initialState = {
     latitude: '',
@@ -9,8 +29,10 @@ const StoreProvider = ({ children }) => {
     coffeeStores: [],
   };
 
+  const [state, dispatch] = useReducer(storeReducer, initialState);
+
   return (
-    <StoreContext.Provider value={{ state: initialState }}>
+    <StoreContext.Provider value={{ state, dispatch }}>
       {children}
     </StoreContext.Provider>
   );
