ef25d50a4eef7056bf7321fbed738fcc7f113d97
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-09 11:33:50 +0200

    Set up airtable

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,8 @@
+const Airtable = require('airtable');
+const base = new Airtable({
+  apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+}).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_KEY);
+
+const table = base('coffee-stores');
+
+console.log(table);
